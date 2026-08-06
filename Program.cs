9ae71a2fa6fc8ae3f86bc2b59322bb85b68df39d
Aman Rai <119662766+Amanrai7905@users.noreply.github.com>
2026-08-06 17:43:50 +0530

    db migrate temprory commnet

--- Program.cs
+++ Program.cs
@@ -118,7 +118,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    Console.WriteLine(builder.Configuration.GetConnectionString("AIChatDBConnection"));
+    //db.Database.Migrate();
 }
 
 
